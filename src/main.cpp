86d1d04ca158d1d543e2f532cd1b45e196c5a9b8
AlwaysFr3sh <shimtucker22187@hotmail.com>
2022-04-16 15:18:43 +1000

    coordinates change

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+// Converts X coordinate from system where 0 is at the center of the screen to the native system where 0 is the top left corner
+int ConvertXCoordinate(int x) {
+  return 500 + x;
+}
+
+// Converts Y coordinate from system where 0 is at the center of the screen to the native system where 0 is the top left corner
+int ConvertYCoordinate(int y) {
+  return 500 - y;
+}
+
 // Wrapper around SDL_RenderDrawPoint() 
 void DrawPoint(SDL_Renderer* renderer, int x, int y, int color[]) {
+  // Convert from center zero coordinates to corner coordinates
+  x = ConvertXCoordinate(x);
+  y = ConvertYCoordinate(y);
+  // Draw
   SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], 255);
   SDL_RenderDrawPoint(renderer, x, y);
 }
@@ -15,13 +29,16 @@ void FillScreen(SDL_Renderer* renderer, int color[]) {
 
 // Draws Line TODO: Implement
 void DrawLine(SDL_Renderer* renderer, int x0, int y0, int x1, int y1) {
+
   return;
+
 }
 
 // Main program loop TODO: add loop
 void ProgramLoop(SDL_Renderer* renderer) {
   int black[3] = {0, 0, 0};
   int white[3] = {255, 255, 255};
+  int red[3] = {255, 0, 0};
   while (true) {
     SDL_Event event;
     if (SDL_PollEvent(&event)) {
@@ -32,6 +49,8 @@ void ProgramLoop(SDL_Renderer* renderer) {
     }
     FillScreen(renderer, black);
     DrawPoint(renderer, 100, 100, white); 
+    DrawPoint(renderer, 100, 200, white);
+    DrawPoint(renderer, 1, 1, red);
     SDL_RenderPresent(renderer);
   }
 }
